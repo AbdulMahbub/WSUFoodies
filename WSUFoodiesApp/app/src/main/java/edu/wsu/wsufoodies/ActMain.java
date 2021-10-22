0498bc3b3372d4529170e629fab22be02c563efc
Abdul Mahbub <go4374@wayne.edu>
2021-10-22 18:52:52 -0400

    Default app after clicking on 'Empty Activity' in Android Studio

---
+++ WSUFoodiesApp/app/src/main/java/edu/wsu/wsufoodies/ActMain.java
@@ -0,0 +1,14 @@
+package edu.wsu.wsufoodies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.laymain);
+    }
+}
