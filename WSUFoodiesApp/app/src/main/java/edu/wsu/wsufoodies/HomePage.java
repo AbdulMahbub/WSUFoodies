b1367e1dd0497b003cafec5431a8ddb1adcd1a5f
Abdul Mahbub <go4374@wayne.edu>
2021-12-05 19:38:18 -0500

    Added login to work with API and have it display name after successful login. Also changed some ID names

--- WSUFoodiesApp/app/src/main/java/edu/wsu/wsufoodies/HomePage.java
+++ WSUFoodiesApp/app/src/main/java/edu/wsu/wsufoodies/HomePage.java
@@ -5,24 +5,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class HomePage extends AppCompatActivity {
+    String userName;
+    TextView welcomeMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        // Retrieve name from successful login
+        userName= getIntent().getStringExtra("fullName");
+
+        // Connect controls to change text
+        welcomeMessage= findViewById(R.id.welcomeText);
+
+        // Display text retrieved from login screen
+        welcomeMessage.setText("Welcome back, "+userName+"!");
+
     }
 
     public void goHome(View v){
-        startActivity(new Intent(HomePage.this, HomePage.class));
+        String name = userName;
+        Intent myIntent = new Intent(HomePage.this, HomePage.class);
+        myIntent.putExtra("fullName", name);
+        startActivity(myIntent);
     }
 
     public void goProfile(View v){
-        startActivity(new Intent(HomePage.this, ProfilePage.class));
+
+        String name = userName;
+        Intent myIntent = new Intent(HomePage.this, ProfilePage.class);
+        myIntent.putExtra("fullName", name);
+        startActivity(myIntent);
     }
 
     public void goAdd(View v){
-        startActivity(new Intent(HomePage.this, AddPage.class));
+        String name = userName;
+        Intent myIntent = new Intent(HomePage.this, AddPage.class);
+        myIntent.putExtra("fullName", name);
+        startActivity(myIntent);
     }
 }
